3b977552766e4bc504da68ceaa1252db31ee62d8
José Ithalo <jose_ithalo@hotmail.com>
2023-05-10 15:20:39 -0300

    Implementação de funções para as rotas

---
+++ src/controladores/carros.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+
+
+export const listarCarros = (req: Request, res: Response) => {
+
+    return res.status(200).json('Listagens de carros');
+
+}
+
+export const cadastrarCarros = (req: Request, res: Response) => {
+
+}
+
+export const detalharCarros = (req: Request, res: Response) => {
+
+}
+
+export const atualizarCarros = (req: Request, res: Response) => {
+
+}
+
+export const excluirCarros = (req: Request, res: Response) => {
+
+}
